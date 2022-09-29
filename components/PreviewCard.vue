85124bea920fc6e212ef10bfc4bd28c4a7ea3aa2
xijieyin <xijieyin@gmail.com>
2022-09-29 14:28:53 +0800

    feat: add preview card

---
+++ components/PreviewCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="preview-card"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PrevireCard',
+})
+</script>
+
+<style lang="scss" scoped>
+.preview-card {
+}
+</style>
